3f8b8d14312be75ee81d62288ab8268641477373
sruizcarmona <sruizcarmona@gmail.com>
2024-07-23 22:53:35 +1000

    update getfilesybzip function

--- src/getFilesZipByID.R
+++ src/getFilesZipByID.R
@@ -6,11 +6,16 @@
 ## output dlist of files
 ############################################################################################################
 
-get_list_files <- function(id, zipfile) {
+get_list_files <- function(zipfile) {
   files_in_zip <- unzip(zipfile, list = TRUE) %>% 
-    filter(Length > 0, !str_detect(Name, "__MACOSX")) %>% 
-    arrange(Name) %>% 
+    filter(Length > 0, !str_detect(Name, "__MACOSX"), !str_detect(Name, ".DS_Store")) %>%
+    arrange(Name) 
+  return(files_in_zip)
+}
+
+get_files_id <- function(id, zipfile) {
+  files_in_zip <- get_list_files(zipfile) %>% 
     pull(Name)
-  ath_files <- files_in_zip[str_detect(files_in_zip, paste0(id))]
-  return(ath_files)
+  id_files <- files_in_zip[str_detect(files_in_zip, paste0(id))]
+  return(id_files)
 }
