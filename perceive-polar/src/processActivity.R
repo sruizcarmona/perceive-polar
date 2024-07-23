61d276414d3e96cea0b45287e1e87fac07a95dcf
sruizcarmona <sruizcarmona@gmail.com>
2024-07-23 22:52:37 +1000

    update processactivity

--- perceive-polar/src/processActivity.R
+++ perceive-polar/src/processActivity.R
@@ -9,26 +9,26 @@
 process_polarfile <- function(zipfile, filename, id, maxhr) {
   # read file and create polar
   polar_file <- unz(zipfile, filename)
-  polar <- try(readTCX(polar_file),silent=T)
+  polar <- try(readTCX(polar_file, filename), silent = T)
   
   
   ###################
   # check errors in file or before processing the activity
   # skip files with errors
   if (class(polar) == "try-error"){
-    act.err <- onerow.df(c(id,rep('file error (reading)', length(act.err.names)-2),file), act.err.names) %>% mutate(date = NA_Date_)
+    act.err <- onerow.df(c(id,rep('file error (reading)', length(act.err.names)-2),filename), act.err.names) %>% mutate(date = NA_Date_)
     return(act.err)
   }
   
   # discard if there are no records or it's shorter than a minute
   if (is.null(dim(polar)) || dim(polar)[1] < 60) {
-    act.err <- onerow.df(c(id,rep('activity error (short/no data)',length(act.err.names)-2),file), act.err.names) %>% mutate(date = NA_Date_)
+    act.err <- onerow.df(c(id,rep('activity error (short/no data)',length(act.err.names)-2),filename), act.err.names) %>% mutate(date = NA_Date_)
     return(act.err)
   }
   
   # discard if activity contains more than 1 row
   # if(length(polar$session$sport) > 1 | length(polar$sport$sport) > 1) {
-  #   act.err <- onerow.df(c(id.id,rep('activity error (multiple sports/rows)',length(act.err.names)-2),file), act.err.names) %>% mutate(date = NA_Date_)
+  #   act.err <- onerow.df(c(id.id,rep('activity error (multiple sports/rows)',length(act.err.names)-2),filename), act.err.names) %>% mutate(date = NA_Date_)
   #   return(act.err)
   # }
   # PERCEIVE comment
@@ -42,22 +42,22 @@ process_polarfile <- function(zipfile, filename, id, maxhr) {
   # check errors in activity
   # skip files with errors in processing activity
   if (class(act) == "try-error"){
-    act.err <- onerow.df(c(id, rep('activity error (unknown)', length(act.err.names) - 2),file), act.err.names) %>% mutate(date = NA_Date_)
+    act.err <- onerow.df(c(id, rep('activity error (unknown)', length(act.err.names) - 2),filename), act.err.names) %>% mutate(date = NA_Date_)
     return(act.err)
   }
   # act hr higher than maxhr
   # if(!is.na(act$maxhr_activity) & act$maxhr_activity > act$maxhr_participant) {
-  #   act.err <- onerow.df(c(id,rep('activity error (maxHR too high)',length(act.err.names)-2),file),act.err.names)
+  #   act.err <- onerow.df(c(id,rep('activity error (maxHR too high)',length(act.err.names)-2),filename),act.err.names)
   #   return(act.err)
   # }
   # timestamp missing
   if(is.na(act$date)) {
-    act.err <- onerow.df(c(id,rep('activity error (missing timestamp)',length(act.err.names)-2),file), act.err.names) %>% mutate(date = NA_Date_)
+    act.err <- onerow.df(c(id,rep('activity error (missing timestamp)',length(act.err.names)-2),filename), act.err.names) %>% mutate(date = NA_Date_)
     return(act.err)
   }
   # wrong year (in the future), hard to find an auto fix and only 1 or 2 examples
   if (act$year > as.numeric(format(as.Date(Sys.Date()),"%Y"))) {
-    act.err <- onerow.df(c(id,rep('activity error (wrong year)',length(act.err.names)-2),file), act.err.names) %>% mutate(date = NA_Date_)
+    act.err <- onerow.df(c(id,rep('activity error (wrong year)',length(act.err.names)-2),filename), act.err.names) %>% mutate(date = NA_Date_)
     return(act.err)
   }
   ###################
